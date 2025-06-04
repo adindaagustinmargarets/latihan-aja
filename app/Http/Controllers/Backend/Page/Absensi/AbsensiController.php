bd3d52d61b5f00daf3efaec79289e0aa872d49e4
adindaagustinmargarets <widy45rr@gmail.com>
2025-06-04 19:52:03 +0700

    push

---
+++ app/Http/Controllers/Backend/Page/Absensi/AbsensiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Page\Absensi;
+
+use App\Http\Controllers\Controller;
+use App\Models\Absensi;
+use Illuminate\Http\Request;
+
+class AbsensiController extends Controller
+{
+    public function absensi_json()
+    {
+        $absensi = Absensi::all();
+        return response()->json([
+            'absensi' => $absensi
+        ]);
+    }
+    public function absensi(Request $request)
+    {
+        $absensi = Absensi::create([
+            'nama_lengkap' => $request->nama_lengkap,
+            'waktu' => now(),
+        ]);
+        return response()->json([
+            'success' => 'Berhasil Absensi',
+            'data' => [
+                'nama_lengkap' => $absensi->nama_lengkap,
+                'waktu' => $absensi->waktu,
+            ]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/absensi', [App\Http\Controllers\Backend\Page\Absensi\AbsensiController::class, 'absensi_json']);
+Route::post('/absensi', [App\Http\Controllers\Backend\Page\Absensi\AbsensiController::class, 'absensi']);
